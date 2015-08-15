fba3074caed7d6487722d9e86c68abb604fcc983
Infatum <qwertty459gmail.com>
2015-08-15 17:41:30 +0300

    First commit (uload)

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+    public float size = 0.3f;
+    private Vector3 originalScale;
+
+    void Start()
+	{
+       originalScale = transform.localScale;
+	}
+
+    public float GetSize()
+    {
+        return size;
+    }
+	
+	void Update()
+	{
+        transform.localScale = originalScale * size;
+	}
+
+    public void OnCollisonEnter(Collision col)
+    {
+       
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rb;
+    private int score = 0;
+    private Text gameOver;
+    public float size = 1f;
+    public Vector3 originalScale;
+    private PlayerController playerSize;
+
+    // Use this for initialization
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        score = 0;
+        originalScale = transform.localScale;
+        playerSize = gameObject.GetComponent<PlayerController>();
+    }
+
+    public float GetSize()
+    {
+        return size;
+    }
+    public void ShowScoreText()
+    {
+        
+    }
+    public void Update()
+    {
+        transform.localScale = originalScale * size;
+    }
+    public void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Enemy") && col.gameObject.GetComponent<EnemyController>().GetSize() < playerSize.GetSize())
+        {
+            Debug.Log("Collision with small enemy");
+            col.gameObject.SetActive(false);
+            size = col.gameObject.GetComponent<EnemyController>().GetSize() + playerSize.GetSize() / 2;
+        }
+    }
+}
